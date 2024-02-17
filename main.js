de4b1527687246d14249d9937a1492c57d112cdf
Jim <tjiemin.khiu@nxgen.asia>
2024-02-17 12:00:08 +0800

    Branch: pet-card final

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ async function start() {
   const weatherData = await weatherPromise.json()
 
   const ourTemperature = weatherData.properties.periods[0].temperature
-
   document.querySelector("#temperature-output").textContent = ourTemperature
+
 }
 
 start()
 
-
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petsPromise.json()
-
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
+
+    clone.querySelector(".pet-card").dataset.species = pet.species
 
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
@@ -27,7 +27,7 @@ async function petsArea() {
     if (!pet.photo) pet.photo = "images/fallback.jpg"
 
     clone.querySelector(".pet-card-photo img").src = pet.photo
-    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
 
     wrapper.appendChild(clone)
   })
@@ -61,5 +61,12 @@ function handleButtonClick(e) {
   e.target.classList.add("active")
 
   // actually filter the pets down below
-
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
