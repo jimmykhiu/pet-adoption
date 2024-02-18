cb807305149ee254457327fb6dd5e9830243f963
Jim <tjiemin.khiu@nxgen.asia>
2024-02-18 21:52:04 +0800

    Get from mongoDB

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ async function start() {
 start()
 
 async function petsArea() {
-  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  // const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsPromise = await fetch("https://fanciful-scone-722531.netlify.app/.netlify/functions/pets")
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
